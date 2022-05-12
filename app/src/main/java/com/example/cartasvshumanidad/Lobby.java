b3d34f02e8ca0295888b6c66274fe6a10eb76abe
RalonsoC01 <72815754+RalonsoC01@users.noreply.github.com>
2022-05-12 21:08:59 +0200

    FloatButton y PopUp

--- app/src/main/java/com/example/cartasvshumanidad/Lobby.java
+++ app/src/main/java/com/example/cartasvshumanidad/Lobby.java
@@ -2,9 +2,17 @@ package com.example.cartasvshumanidad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class Lobby extends AppCompatActivity {
 
@@ -13,6 +21,11 @@ public class Lobby extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lobby);
     }
+    private AlertDialog.Builder dialogBuilder;
+    private AlertDialog dialog;
+    private TextView tvPregunta;
+    private Button aceptar;
+    private Button cancel;
 
     public void news (View view)
     {
@@ -24,4 +37,42 @@ public class Lobby extends AppCompatActivity {
         Intent i= new Intent(this,Profile.class);
         startActivity(i);
     }
+
+    public void CrearPartida(View view)
+    {
+        createNewContactDialog();
+    }
+
+    public void createNewContactDialog(){
+        dialogBuilder = new AlertDialog.Builder(this);
+        final View contactPopupView = getLayoutInflater().inflate(R.layout.popup, null);
+        this.tvPregunta=(TextView) contactPopupView.findViewById(R.id.tvPregunta);
+        this.aceptar=(Button) contactPopupView.findViewById(R.id.btnPopUpCrear);
+        this.cancel=(Button) contactPopupView.findViewById(R.id.btnPopUpCancelar);
+
+        this.dialogBuilder.setView(contactPopupView);
+        this.dialog = dialogBuilder.create();
+        dialog.show();
+
+        this.aceptar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                Intent intent = new Intent(Lobby.this, Juego.class);
+
+                dialog.dismiss();
+            }
+        });
+
+        this.cancel.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                Intent intent = new Intent(Lobby.this, Lobby.class);
+
+                dialog.dismiss();
+            }
+        });
+    }
+
 }
