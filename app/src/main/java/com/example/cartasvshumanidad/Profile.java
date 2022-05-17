630f3adb70a6a2e99c9a526e3108e5c2da69e61e
AlexYRaul <103044293+AlexYRaul@users.noreply.github.com>
2022-05-17 19:46:26 +0200

    opcion de cerrar sesion

--- app/src/main/java/com/example/cartasvshumanidad/Profile.java
+++ app/src/main/java/com/example/cartasvshumanidad/Profile.java
@@ -5,13 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Profile extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        TextView mButtonSignOut = (TextView) findViewById(R.id.tvCerrarSesion);
+        mAuth = FirebaseAuth.getInstance();
+
+        mButtonSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               mAuth.signOut();
+               startActivity(new Intent(Profile.this, Login.class));
+               finish();
+            }
+        });
     }
 
     public void news (View view)
@@ -25,9 +40,7 @@ public class Profile extends AppCompatActivity {
         startActivity(i);
     }
 
-    public void CerrarSesion (View view)
-    {
-        Intent i= new Intent(this,Login.class);
-        startActivity(i);
-    }
+
+
+
 }
