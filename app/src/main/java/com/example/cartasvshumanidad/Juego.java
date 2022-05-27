c371eaab840f6e61e964dca8265f0b377c2bb7bd
RalonsoC01 <72815754+RalonsoC01@users.noreply.github.com>
2022-05-27 16:46:39 +0200

    Bloqueo de pantalla de juego

--- app/src/main/java/com/example/cartasvshumanidad/Juego.java
+++ app/src/main/java/com/example/cartasvshumanidad/Juego.java
@@ -2,6 +2,7 @@ package com.example.cartasvshumanidad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 public class Juego extends AppCompatActivity {
@@ -10,5 +11,6 @@ public class Juego extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_juego);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
 }
